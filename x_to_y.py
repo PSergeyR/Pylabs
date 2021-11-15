2fe8566676cee4e49be214e057bcb5dd837bfa9c
PSergeyR <mapf44@mail.ru>
2021-11-15 21:39:04 +0500

    deleted

--- x_to_y.py
+++
@@ -1,9 +0,0 @@
-orig_str = 'jdfhxjhjhdfxjxjhfdxlsdkkx xxx yyy'
-new_str = ''
-for x in orig_str:
-    if x == 'x':
-        new_str += 'y'
-    else:
-        new_str += x
-print(orig_str)
-print(new_str)
