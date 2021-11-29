b715cf88586af044592a420e4b09b6bb54006e72
PSergeyR <mapf44@mail.ru>
2021-11-29 20:47:22 +0500

    после правок

--- lesson8.py
+++ lesson8.py
@@ -1,7 +1,14 @@
-persons = [['Иванов', 'Иван', 1991, 'холост'], ['Петров', 'Петр', 1992, 'разведен'],
-           ['Сидоров', 'Василий', 1996, 'женат'], ['Попов', 'Клим', 1994, 'холост']]
-persons1 = {'Иванов' :{ 'fname': 'Иван', 'bday': '01.01.1991'}, 'Петров': { 'fname': 'Петр', 'bday': '02.02.1992'},
-             'Сидоров':{ 'fname': 'Василий', 'bday': '03.03.1993'},'Попов':{'fname': 'Клим', 'bday': '04.04.1994'}}
+persons = [['Иванов', 'Иван', 1991, 'холост'],
+           ['Петров', 'Петр', 1992, 'разведен'],
+           ['Сидоров', 'Василий', 1996, 'женат'],
+           ['Попов', 'Клим', 1994, 'холост']]
+
+persons1 = {'Иванов': {'fname': 'Иван', 'bday': '01.01.1991'},
+            'Петров': {'fname': 'Петр', 'bday': '02.02.1992'},
+            'Сидоров': {'fname': 'Василий', 'bday': '03.03.1993'},
+            'Попов': {'fname': 'Клим', 'bday': '04.04.1994'}}
+
+
 def old():
     name = 'Имя'
     oldest = persons[0][2]
@@ -9,8 +16,8 @@ def old():
         if persons[x][2] > oldest:
             oldest = persons[x][2]
             name = persons[x][0]
-
     print('Самый старший:', name, 'год рождения:', oldest)
+
 
 def old_dic():
     oldestDic = persons1['Иванов']['bday']
@@ -19,28 +26,20 @@ def old_dic():
             oldestDic = persons1[key]['bday']
     print('Самый старший в словаре:', key, 'год рождения:', oldestDic)
 
+
 old()
 old_dic()
 
-def compare (s1,s2):
-    ngrams = [s1[i:i+3] for i in range(len(s1))]
+
+def compare(s1, s2):
+    ngrams = [s1[i:i + 3] for i in range(len(s1))]
     count = 0
     for ngram in ngrams:
         count += s2.count(ngram)
-    return count/max(len(s1), len(s2))
+    return count / max(len(s1), len(s2))
+
 
 def main():
     s1 = input("Введите первую строку:")
     s2 = input("Введите вторую строку:")
     print(compare(s1, s2))
-
-
-if __name__ == '__main__':
-    main()
-
-
-
-
-
-
-
