b40765dde29ca9434ae8b8d489e5f2a0cd488ce1
PSergeyR <mapf44@mail.ru>
2021-12-07 17:00:25 +0500

    добавление сохранения в json #Видео 11

--- brain_3.py
+++ brain_3.py
@@ -3,14 +3,38 @@ from sys import stderr
 from random import randint
 from pprint import pprint
 from itertools import product
+from json import load, dump
 
 NOT_FOUND = '$'
 MIN_WORD_SIMILARITY = 0
 MIN_QUESTION_SIMILARITY = 0.5
 
+
 class Brain():
     memory = dict()
     answers = ('Да', 'Нет')
+
+    def __init__(self):
+        self.memory = {}
+        try:
+            loaded = load(fp=open('memory.json'))
+            for str_key, val in loaded.items():
+                tuple_key = tuple(str_key.split())
+                self.memory.update({tuple_key: val})
+        except FileNotFoundError:
+            pass
+
+    def save(self):
+        to_save = {}
+        for tuple_key, val in self.memory.items():
+            str_key = re.sub(r'[()\',]+', ' ', str(tuple_key)).strip()
+            to_save.update({str_key: val})
+        dump(
+            obj=to_save,
+            fp=open('memory.json', 'w'),
+            ensure_ascii=0,
+            indent=4
+        )
 
     def __fuzzy_key_search(self, words):
         answer = NOT_FOUND
@@ -25,15 +49,14 @@ class Brain():
         assert answer != NOT_FOUND
         return answer
 
-
     def __compare(self, s1, s2):
         count = 0
-        for ngram in (s1[i:i+3] for i in range(len(s1)-1)):
+        for ngram in (s1[i:i + 3] for i in range(len(s1) - 1)):
             count += s2.count(ngram)
         return count / max(len(s1), len(s2))
 
     def think(self, question):
-        words = tuple(re.sub(r'[!;:?,.-]',' ', question).split())
+        words = tuple(re.sub(r'[!;:?,.-]', ' ', question).split())
         try:
             return "Я уже отвечала, ответ был %s." % self.memory[words]
         except KeyError:
@@ -41,8 +64,6 @@ class Brain():
                 answer = self.__fuzzy_key_search(words)
                 return "Я уже отвечала, ответ был %s." % answer
             except (AssertionError, ZeroDivisionError):
-                answer =self.answers[randint(0, len(self.answers)-1)]
-                self.memory.update({words:answer})
-                return "Мой ответ - %s!" %answer
-
-
+                answer = self.answers[randint(0, len(self.answers) - 1)]
+                self.memory.update({words: answer})
+                return "Мой ответ - %s!" % answer
